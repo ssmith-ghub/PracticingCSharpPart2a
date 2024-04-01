55dfbc141b6ac2874541f1fe4158b7ffae1431b4
Stephen Smith <ssmith_138823@outlook.com>
2024-03-31 22:07:56 -0400

    pushing repo

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello ");
+            Console.WriteLine("world!  How are you?");
+            Console.WriteLine("\tI'm Stephen!");
+            Console.WriteLine("I'mm\b learning C#!");
+            Console.WriteLine("its fun\nbut hard!");
+
+            //This is me practicing commenting out
+            /* this
+             * is 
+             * mutliple 
+             * line
+             * pracitce 
+             * for
+             * comments
+             */
+
+            Console.ReadKey();
+        }
+    }
+}
